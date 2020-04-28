851e0260367cc2304e9748559539f1436c2093c2
Erik Bender <erik.bender@develerik.dev>
2020-04-28 23:49:05 +0200

    add nord theme for kitty

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ git submodule init
 git submodule update
 
 # what directories should be installable
-packages="git gnupg ssh tmux"
+packages="git gnupg ssh tmux kitty"
 
 echo ""
 echo "Stowing apps for user: $(whoami)"
