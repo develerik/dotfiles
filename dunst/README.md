4d0dad18147d5a76dedf360152af63f2adc2101c
Erik Bender <erik.bender@develerik.dev>
2020-04-29 20:29:03 +0200

    add dunst configuration

---
+++ dunst/README.md
@@ -0,0 +1,13 @@
+# Dunst
+
+## Table of Contents
+
+1. [Features](#Features)
+
+## Features
+
+- show notifications only on main screen
+- uses nord theme
+- disables all shortcuts
+- special setup for:
+  - spotify
--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ git submodule init
 git submodule update
 
 # what directories should be installable
-packages="git gnupg ssh tmux kitty chromium rofi"
+packages="git gnupg ssh tmux kitty chromium rofi dunst"
 
 echo ""
 echo "Stowing apps for user: $(whoami)"
