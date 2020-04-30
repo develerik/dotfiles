f78acbaaab06b0f97187d735ce603093884e1571
Erik Bender <erik.bender@develerik.dev>
2020-04-30 21:28:35 +0200

    fix mediaplayer block (amazon prime)

--- i3/.config/i3blocks/blocks/mediaplayer.sh
+++ i3/.config/i3blocks/blocks/mediaplayer.sh
@@ -3,6 +3,15 @@
 artist=$(playerctl metadata artist)
 title=$(playerctl metadata title)
 
+REG_AMZ="^Amazon\.de: ([a-zA-Z!\[\]. /üöä-]+) ansehen \| Prime Video$"
+
+# fix amazon prime video
+case $title in
+  "Amazon.de"*)
+    title="$(echo "${title}" | pcregrep -o1 "$REG_AMZ")"
+    ;;
+esac
+
 result="${artist}"
 
 [ -n "$artist" ] && [ -n "$title" ] && result="${result} - ${title}"
