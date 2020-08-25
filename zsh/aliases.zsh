2b16b474e92ef4b973d106b735790d31b9d6cabd
Erik Bender <erik.bender@develerik.dev>
2020-08-25 08:52:34 +0200

    add .exe extraction to extract alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -115,6 +115,7 @@ function extract() {
       (*.tgz) tar xzf $1 ;;
       (*.zip) unzip $1 ;;
       (*.Z) uncompress $1 ;;
+      (*.exe) cabextract $1 ;;
       (*) echo "'$1' cannot be extracted via extract()" ;;
     esac
   else
